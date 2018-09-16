81f0e93cdd457675925efa1a098a911a0835f99e
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-09-16 09:46:04 +0530

    Home Page created

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-navbar></app-navbar>
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/Navbar';
+export default {
+  components:{
+    'app-navbar' : Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Vue App</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Songs</a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+</template>
+
+<script></script>
+
+<style></style>
