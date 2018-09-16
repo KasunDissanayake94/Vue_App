a08b267fa70b960849571d99191d4b3a28592eb3
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-09-16 13:59:50 +0530

    Get json data  tracks from last.fm

--- src/components/SearchArea.vue
+++ src/components/SearchArea.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="form-group">
-    <label for="exampleInputEmail1">Search Music By Title</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="songInputEmail1">Search Music By Title</label>
+    <input type="text" class="form-control" id="songInputEmail1" aria-describedby="emailHelp" placeholder="Enter here" @input="keyPressed">
     <small id="emailHelp" class="form-text text-muted">You can see your search result here</small>
   </div>
 
 </template>
-<script></script>
+<script>
+  import axios from 'axios';
+  export default {
+
+
+    methods:{
+      keyPressed(event){
+        var key = event.target.value;
+
+        // Make a request for a user with a given ID
+        axios.get("http://ws.audioscrobbler.com/2.0/?method=track.search&track="+ key +"&api_key=208408abe58141dbff3826cafc4a65b9&format=json")
+          .then(response => {
+            console.log(response.data.results.trackmatches.track)
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      }
+    }
+  }
+</script>
